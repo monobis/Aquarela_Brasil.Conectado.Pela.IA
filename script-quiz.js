ac401d943934be2635085767ea83ffd8833102b7
monobis <rafaelasayurimonobi@gmail.com>
2025-10-22 19:48:23 -0300

    Update script-quiz.js

--- script-quiz.js
+++ script-quiz.js
@@ -34,5 +34,6 @@ document.getElementById("quizForm").addEventListener("submit", (e) => {
 
   localStorage.setItem("respostasQuiz", JSON.stringify(respostas));
 
-  window.location.href = "../recomendacoes/recomendacoes.html";
+  window.location.href = "recomendacoes.html";
 });
+
